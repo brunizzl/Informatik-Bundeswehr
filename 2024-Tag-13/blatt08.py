0cf1955bae93d317c56a8b6235ad3760598271d5
Bruno Borchardt <bruno.borchardt@tuhh.de>
2024-07-05 11:36:18 +0200

    stand nach heute

--- 2024-Tag-13/blatt08.py
+++ 2024-Tag-13/blatt08.py
@@ -15,11 +15,46 @@ def tausche_test() -> list[int]:
     return liste
 
 # Aufgabe 3
-# vorsicht: funktioniert noch nicht :0
 def bubblesort(liste: list[int]):
-    for i in range(len(liste) - 1):
-        i1 = i
-        i2 = i + 1
-        if liste[i1] > liste[i2]:
-            tausche(liste, i1, i2)
+    for _ in range(len(liste)):
+        for i in range(len(liste) - 1):
+            i1 = i
+            i2 = i + 1
+            if liste[i1] > liste[i2]:
+                tausche(liste, i1, i2)
+
+# diese funktion produziert eine sortierte liste mit gleichen
+# eintraegen wie das argument, zerstoert dabei allerdings das
+# argument (am ende ist die variable "liste" leer).
+# im gegensatz zu der bubblesortvariante aus aufgabe 3 wird hier
+# nicht das jetzt-groesste an den richtigen ort gesetzt, sondern
+# dass jetzt-kleinste.
+def bubblesort_variante_marcel(liste: list[int]) -> list[int]:
+    ergebnis = []
+    for _ in range(len(liste)):
+        kleinster = liste[0]
+        i_kleinster = 0
+        for i in range(1, len(liste) - 1):
+            if liste[i] < kleinster:
+                kleinster = liste[i]
+                i_kleinster = i
+        # notwendig, damit wir nicht jeden durchlauf den selben
+        # eintrag finden.
+        liste.pop(i_kleinster)
+        ergebnis.append(kleinster)
+    return ergebnis
+
+# Aufgabe 4
+def insertionsort(liste: list[int]) -> list[int]:
+    ergebnis = []
+    for x in liste:
+        i = len(ergebnis)
+        while i > 0 and ergebnis[i - 1] > x:
+            i -= 1
+        ergebnis.insert(i, x)
+        
+    return ergebnis
+
+
+
 
